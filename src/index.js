b4b995321e58c241e90a61ff00c2a49a7e5a8591
LucasJappert <lucasnicolasjappert@gmail.com>
2020-11-17 17:42:54 -0300

    Ajuste CORS

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ const Config = require('../config.js');
 //settings
 app.set('json spaces', 2);
 
+
+// Configurar cabeceras y cors
+const cors = require("cors");
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+//cors
+app.use(cors());
+
 //middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
